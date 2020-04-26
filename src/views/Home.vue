594b89aea83f039c064264fe95743b1b72082658
Magdiel Campelo <magdiel.camp@gmail.com>
2020-04-26 09:45:04 +0100

    adds kinectic balls with random colors based on perlin noise

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
+  <div class="home relative overflow-hidden">
     <kinesis-container>
-      <div class="w-screen h-screen flex justify-center items-center bg-blue-gray relative">
-        <kinesis-element :strength="10" type="depth">
+      <div class="w-screen h-screen flex justify-center items-center bg-blue-gray z-0">
+        <kinesis-element :strength="10" type="depth" class="z-50">
           <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
             <div class="px-6 py-4 flex flex-row">
               <div class="flex justify-center items-center">
@@ -22,6 +22,17 @@
             </div>
           </div>
         </kinesis-element>
+        <kinesis-element
+          v-for="(ball,index) in balls"
+          :key="index"
+          class="absolute z-10"
+          :style="{top:ball.y+'px',left:ball.x+'px',}"
+        >
+          <div
+            class="rounded-full border"
+            :style="{width: 2*ball.r+'px',height: 2*ball.r+'px', 'border-color': ball.color, 'border-width':ball.border+'px'}"
+          >&nbsp;</div>
+        </kinesis-element>
       </div>
     </kinesis-container>
   </div>
@@ -29,6 +40,8 @@
 
 <script>
 import { KinesisContainer, KinesisElement } from "vue-kinesis";
+import flatcolor from "@/utils/flatcolor";
+import perlin from "@/utils/perlin";
 
 export default {
   name: "Home",
@@ -36,8 +49,25 @@ export default {
     KinesisContainer,
     KinesisElement
   },
+  data: () => ({
+    balls: []
+  }),
   created() {
-    console.log(this);
+    let number = Math.floor(Math.random() * 10 + 3);
+    // noise.seed(Math.random);
+    let noise = perlin.noise;
+    noise.seed(Math.random);
+    for (var i = 0; i < number; i++) {
+      let x = Math.floor(Math.random() * (window.innerWidth - 300) + 150);
+      let y = Math.floor(Math.random() * (window.innerHeight - 300) + 150);
+      this.balls.push({
+        x,
+        y,
+        r: Math.floor(Math.random() * 150),
+        border: Math.floor(Math.random() * 5 + 1),
+        color: flatcolor(Math.abs(noise.perlin2(x / 100, y / 100)) * 360).hex
+      });
+    }
   }
 };
 </script>
