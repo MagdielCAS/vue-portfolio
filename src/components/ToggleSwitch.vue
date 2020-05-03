92e98bc13c77a3e8e4348975843295beed77f18b
Magdiel Campelo <magdiel.camp@gmail.com>
2020-05-03 17:06:28 +0100

    toggle component

---
+++ src/components/ToggleSwitch.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="px-2">
+    <div
+      class="rounded-full p-1 bg-manatee cursor-pointer ease duration-300"
+      :class="{
+        'bg-manatee': !content,
+        'bg-munsell': content
+      }"
+      @click="content = !content"
+    >
+      <div
+        class="rounded-full border-8 ease duration-300"
+        :class="{
+          'border-white mr-4': !content,
+          'border-tea-green ml-4': content
+        }"
+      >&nbsp;</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToggleSwitch",
+  props: ["value"],
+  computed: {
+    content: {
+      get() {
+        return !!this.value;
+      },
+      set(content) {
+        this.$emit("input", content);
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
