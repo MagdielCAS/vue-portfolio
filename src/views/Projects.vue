b7b532ef346fd66ca65709873ce27102d921bd82
Magdiel Campelo <magdiel.camp@gmail.com>
2020-05-29 23:03:22 +0100

    under development warning

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="relative overflow-hidden">
-    <div class="w-screen h-screen flex justify-center items-center z-0">
+  <div class="h-screen relative overflow-hidden">
+    <div class="text-lg text-white font-medium p-4">
+      {{ $t("warnings.development") }}
+    </div>
+    <div class="w-screen flex justify-center items-center z-0">
       <MaurerRose></MaurerRose>
     </div>
   </div>
